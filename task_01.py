0a4424ed1328e30497c0104773bf11fb0faa89fb
Laura Pomeroy <laura.pomeroy@spsmail.cuny.edu>
2016-09-16 20:00:13 +0000

    task_01.py Warmup

--- task_01.py
+++ task_01.py
@@ -10,28 +10,21 @@ def know_what_i_mean(wink, numwink=2):
     Args:
         retstr (mixed): Arg to be retstr
         nudges (mixed): Arg to multiply and remove first and last letter.
-        winks (mixed): Arg to be multiplied and stripped of first and last letter.
+        winks (mixed): Arg to be multiplied and stripped of first and last
+        letter.
         numwink (str, optional): A numerical string. Default: 2
 
     Returns:
-        str: All arguments concatenated with commas.
+        str: A function that knows what you mean.
 
-
-    Raises:
-        AttributeError: 'int' object has no attribute 'strip'.
 
     Examples:
-
-        >>> know_what_i_mean(2,3)
-
-        Traceback (most recent call last):
-        File "<pyshell#0>", line 1, in <module>
-        know_what_i_mean(2,3)
-        File "/home/vagrant/Desktop/is210-week-04-warmup/task_01.py", line 26, in know_what_i_mean
-        winks = (wink * numwink).strip()
-        AttributeError: 'int' object has no attribute 'strip'>>> know_what_i_mean(2, 3) 
+        >>> know_what_i_mean('wink', 2)
+        'Know what I mean? winkwink, nudgenudge'
+        >>> know_what_i_mean('numwink', 3)
+        'Know what I mean? numwinknumwinknumwink, nudgenudgenudge'
     """
     winks = (wink * numwink).strip()
     nudges = ('nudge' * numwink).strip()
-    retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
+    retstr = 'Know what I mean? {0}, {1}'.format(winks, nudges)
     return retstr
